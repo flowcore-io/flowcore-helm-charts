0de2f568abafce0701e1633949309fba9eb94ede
Julius á Rógvi Biskopstø <jbiskur@gmail.com>
2023-01-17 11:06:27 +0000

    fix(flowcore-microservices): fixed syntax error in deployment.yaml

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,46 @@
 path=$1
 version_type=$2
 
+echo "Linting $path with test values"
+helm lint --debug --values "$path"/tests/test-values.yaml "$path"
+
+# If linting fails, exit
+if [ $? -ne 0 ]; then
+    echo "Linting failed"
+    exit 1
+fi
+
+echo "Deploying new $version_type for $path"
+
 # Update version in Chart.yaml file
 if [ "$version_type" == "patch" ]; then
-  sed -i 's/version: .*/version: '$(cat $path/Chart.yaml | grep version: | awk '{print $2}')'.1/' $path/Chart.yaml
+  newVersion=$(echo $(cat "$path"/Chart.yaml | grep version: | awk '{print $2}') | awk -F '.' '{print $1 "." $2 "." $3+1}')
+
+  echo "New version: $newVersion"
+
+  sed -i.bak "s/^version:.*/version: $newVersion/" "$path"/Chart.yaml
+  sed -i.bak "s/^appVersion:.*/appVersion: $newVersion/" "$path"/Chart.yaml
+  rm "$path"/Chart.yaml.bak
 elif [ "$version_type" == "minor" ]; then
-  sed -i 's/version: .*/version: '$(echo $(cat $path/Chart.yaml | grep version: | awk '{print $2}') | awk -F '.' '{print $1 "." $2+1 "." 0}')'/' $path/Chart.yaml
+  newVersion=$(echo $(cat "$path"/Chart.yaml | grep version: | awk '{print $2}') | awk -F '.' '{print $1 "." $2+1 "." 0}')
+
+  echo "New version: $newVersion"
+
+  sed -i.bak "s/^version:.*/version: $newVersion/" "$path"/Chart.yaml
+  sed -i.bak "s/^appVersion:.*/appVersion: $newVersion/" "$path"/Chart.yaml
+  rm "$path"/Chart.yaml.bak
 else
-  sed -i 's/version: .*/version: '$(echo $(cat $path/Chart.yaml | grep version: | awk '{print $2}') | awk -F '.' '{print $1+1 "." 0 "." 0}')'/' $path/Chart.yaml
+  newVersion=$(echo $(cat "$path"/Chart.yaml | grep version: | awk '{print $2}') | awk -F '.' '{print $1+1 "." 0 "." 0}')
+
+  echo "New version: $newVersion"
+
+  sed -i.bak "s/^version:.*/version: $newVersion/" "$path"/Chart.yaml
+  sed -i.bak "s/^appVersion:.*/appVersion: $newVersion/" "$path"/Chart.yaml
+  rm "$path"/Chart.yaml.bak
 fi
 
 # Package chart
-helm package $path
+helm package "$path"
 
 # Update index
 helm repo index --url  https://flowcore-io.github.io/flowcore-helm-charts/ .
